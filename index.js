fc64e785134504d8efd12910f216b49b93f7f20e
Weverton <wevertondonato4@gmail.com>
2026-03-25 17:09:00 -0300

    Tela de Produção base

--- index.js
+++ index.js
@@ -4,21 +4,48 @@ const voltarLogin = () => {
 
 const logar = () => {
     window.location.href = "telaTop.htm"
-}
+
+
+
 const sairtop = () => {
     window.location.href = "login.htm"
 }
 
+const voltarTop = () => {
+    window.location.href =     
+}}
 
-const botao = document.getElementById("btsalvar");
-if (botao) {
-    botao.addEventListener("click", function() {
-        const toastEl = document.getElementById("notificacao");
-        if (toastEl) {
-            const toast = new bootstrap.Toast(toastEl);
-            toast.show();
-        }
-    });
+
+
+
+const limparFormularioOP = () => {
+    document.getElementById("numOP").value = "";
+    document.getElementById("produto").value = "";
+    document.getElementById("quantidade").value = "";
+    document.getElementById("prazo").value = "";
+    document.getElementById("status").value = "";
+    document.getElementById("responsavel").value = "";
+    document.getElementById("equipamento").value = "";
+   
 }
 
 
+
+const editarOP = (botao) => {
+    
+}
+
+const excluirOP = (botao) => {
+    if (confirm("Tem certeza que deseja excluir esta OP?")) {
+        botao.closest("tr").remove
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoSalvar = document.getElementById("salvarOP");
+    
+    if (botaoSalvar) {
+        botaoSalvar.addEventListener("click", salvarOP);
+    }
+    
+})
